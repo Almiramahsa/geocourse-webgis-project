cd31af3cbabffaefc36f73c621f322e151b11d1c
Almira Mahsa <almiramahsa9@gmail.com>
2023-10-28 15:24:53 +0800

    Step 5 - Membuat Form Tambah Fasilitas

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,17 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path
-from bikini_bottom.views import home, home_map_api, custom_map_api  # Import custom_map_api
+from bikini_bottom.views import home, home_map_api, custom_map_api, facility_form_add
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('api/home-map/', home_map_api),
+    path('', home, name='home'),
+    path('api/home-map/', home_map_api, name='home_api'),
     path('api/custom-map/', custom_map_api, name='custom_api'),
-
+    path('facility/add/', facility_form_add, name='facility_form_add')
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
