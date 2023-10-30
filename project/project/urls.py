c4871d8e4371a2e8541f4f222b35c2083cc297b1
Almira Mahsa <almiramahsa9@gmail.com>
2023-10-30 10:37:54 +0800

    Step 8 - Update & Delete Objek Fasilitas

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,6 @@
-
 from django.contrib import admin
-from django.urls import path
-from bikini_bottom.views import home, home_map_api, custom_map_api, facility_form_add
+from django.urls import path, include
+from bikini_bottom.views import home, home_map_api, custom_map_api, facility_form_add, facility_form_update, facility_form_delete, facility_list
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,11 +9,14 @@ urlpatterns = [
     path('', home, name='home'),
     path('api/home-map/', home_map_api, name='home_api'),
     path('api/custom-map/', custom_map_api, name='custom_api'),
+    path('facility/', facility_list, name='facility_list'),
     path('facility/add/', facility_form_add, name='facility_form_add'),
-    path('facility/', facility_list, name='facility_list')
+    path('facility/update/<int:pk>/', facility_form_update, name='facility_form_update'),
+    path('facility/delete/<int:pk>/', facility_form_delete, name='facility_form_delete'),
 
+    # Sistem Authentication
+    path('', include('django.contrib.auth.urls')),
 ]
-
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
